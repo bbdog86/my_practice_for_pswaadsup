6ad10f03de3fe7790ac5244bcbb91247c0dbcba8
bbdog86 <bbdog86@hotmail.com>
2021-12-29 10:22:36 +0800

    Add files via upload

--- 3_10_5_test.py
+++ 3_10_5_test.py
@@ -1,14 +1,36 @@
-import pytest
+from icecream import ic
 class Queue:
-    def __init__:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
     
-    def enqueue:
-    
-    def dequeue:
-    
-    def isEmpty:
-    
-    def Size:
-    
-    
- 
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        x=self.items[0]
+        del self.items[0]
+        return x
+
+    def size(self):
+        return len(self.items)
+
+
+q = Queue()
+ic(q.isEmpty())
+
+q.enqueue(4)
+q.enqueue('dog')
+q.enqueue(True)
+
+ic(q.size())
+ic(q.isEmpty())
+
+q.enqueue(8.4)
+
+ic(q.dequeue())
+ic(q.dequeue())
+
+ic(q.size())
